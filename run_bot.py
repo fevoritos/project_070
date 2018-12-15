db823c90ac4db77cf620b639cd626d3bf46ba190
fevoritos <fevoritos@mail.ru>
2018-12-15 16:42:44 +0300

    Новый бот

--- run_bot.py
+++ run_bot.py
@@ -10,23 +10,27 @@ def write_msg(user_id, text):
 
 
 vk_bot = vk_api.VkApi(token=TOKEN)
-long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts' : 1, 'lp_version': 3})
+long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
 server, key, ts = long_poll['server'], long_poll['key'], long_poll['ts']
 print("готов к работе")
 # + str(long_poll))
 
 while True:
     long_poll = requests.get(
-        'https://{server}?act={act}&key={key}&ts={ts}&wait=500'.format(server=server,
-                                                                       act='a_check',
-                                                                       key=key,
-                                                                       ts=ts)).json()
+        'https://{server}?act={act}&key={key}&ts={ts}&wait=2000'.format(server=server,
+                                                                        act='a_check',
+                                                                        key=key,
+                                                                        ts=ts)).json()
     update = long_poll['updates']
     if update[0][0] == 4:
-        print(update)
+        if update[0][6] == 'стратегия':
+            print(update)
+            user_id = update[0][3]
+            user_name = vk_bot.method('users.get', {'user_ids': user_id})
+            write_msg(user_id, 'поиграй в League of Legends')
         user_id = update[0][3]
         user_name = vk_bot.method('users.get', {'user_ids': user_id})
-        write_msg(user_id, 'привет , ' + (user_name[0]['first_name']))  # сообщение пользователю
+        write_msg(user_id, 'привет , ' + (user_name[0]['first_name']) + 'какой жанр игр предпочитаешь?')  # сообщение пользователю
         print(str(user_name[0]['first_name']) + ' ' +
               str(user_name[0]['last_name']) + ' написал(а) боту - ' + str(update[0][6]))  # сообщение нам
         # Меняем ts для следующего запроса
